5a890f25ab6b75e48c24013dc5b94e1617544b32
Alan <alan.turtleboy@gmail.com>
2020-03-22 14:27:34 -0600

    Added Town scene and Scene Switch script

---
+++ Assets/Scripts/Town/SceneSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        } else {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
