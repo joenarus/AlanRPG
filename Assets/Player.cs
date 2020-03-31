60f2ecaa07e595dbd9d73a3fb533174bb7cf148a
Joe Narus <PMTC86@motorolasolutions.com>
2020-03-31 12:49:08 -0600

    Added Player Saving
    Saves player data to a binary file

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level;
+    public int health;
+    public int mana;
+    public int strength;
+    public int wisdom;
+    public int dexterity;
+    public int charisma;
+    public int constitution;
+    public int intelligence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
