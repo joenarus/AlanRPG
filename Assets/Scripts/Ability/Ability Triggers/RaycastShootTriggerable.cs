cabbd4feb4cd52ed5515ea063f3a76db090e86c8
Joe Narus <PMTC86@motorolasolutions.com>
2020-03-31 12:18:08 -0600

    Remove Fire Functionality
    Will update with correct functionality later

--- Assets/Scripts/Ability/Ability Triggers/RaycastShootTriggerable.cs
+++ Assets/Scripts/Ability/Ability Triggers/RaycastShootTriggerable.cs
@@ -9,51 +9,27 @@ public class RaycastShootTriggerable : MonoBehaviour
     [HideInInspector] public float hitForce = 0f;
     [HideInInspector] public LineRenderer rayLine;
     public Transform spawnPoint;
-
     private Camera tpCam;
+    private GameObject player;
     private WaitForSeconds duration = new WaitForSeconds(.07f);
     private new AudioSource audio;
 
     public void Initialize()
     {
         rayLine = GetComponent<LineRenderer>();
-
+        player = GameObject.Find("Player");
         //audio = GetComponent<AudioSource>();
-
-        tpCam = GetComponentInParent<Camera>();
+        tpCam = GameObject.Find("TPCamera").GetComponent<Camera>();
     }
 
+    //TODO: FIGURE OUT how to shoot from player forward
     public void Fire()
     {
-        Vector3 rayOrigin = tpCam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
-        Debug.DrawRay(rayOrigin, tpCam.transform.forward * range, Color.green);
-        RaycastHit hit;
-
-        StartCoroutine(ShotEffect());
-
-        rayLine.SetPosition(0, spawnPoint.position);
-
-        if (Physics.Raycast(rayOrigin, tpCam.transform.forward, out hit, range))
-        {
-            rayLine.SetPosition(1, hit.point);
-
-            // Get Reference to script associated with hit
-            // Entity health = hit.collider.GetComponent<Entity>();
-            // if (health != null)
-            // {
-            //     health.Damage(damage);
-            // }
-            // if (hit.rigidbody != null)
-            // {
-            //     hit.rigidbody.AddForce(-hit.normal * hitForce);
-            // }
-        }
-        else {
-            rayLine.SetPosition(1, tpCam.transform.forward * range);
-        }
+        
     }
 
-    private IEnumerator ShotEffect() {
+    private IEnumerator ShotEffect()
+    {
         rayLine.enabled = true;
         yield return duration;
         rayLine.enabled = false;
