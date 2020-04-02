6e497260b0a5da991e857d5ffaa7d11b3de3726d
Joe Narus <PMTC86@motorolasolutions.com>
2020-04-02 11:58:06 -0600

    Point+click + packge import
    Imported package

---
+++ Assets/Scripts/CameraControllerPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerPoint : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoon = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
+    public float yawSpeed = 100f;
+    private float currentZoom = 10f;
+    private float currentYaw = 0f;
+    
+    void Update() {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoon, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+    void LateUpdate() {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
