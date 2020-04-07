40b6227e19bb9b5d92c296a92299dfc0ee0fcec9
Joe Narus <PMTC86@motorolasolutions.com>
2020-04-07 10:15:21 -0600

    Combat and enemy tracking
    whew

---
+++ Assets/Scripts/Controllers/CharacterCombat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour
+{
+    public float attackSpeed = 1f;
+    private float attackCoolDown = 0f;
+
+    public event System.Action OnAttack;
+
+    public float attackDelay = .6f;
+    CharacterStats myStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStats = GetComponent<CharacterStats>();
+    }
+
+    void Update()
+    {
+        attackCoolDown -= Time.deltaTime;
+    }
+
+    public void Attack(CharacterStats targetStats)
+    {
+        if (attackCoolDown <= 0f)
+        {
+            StartCoroutine(DoDamage(targetStats, attackDelay));
+            if (OnAttack != null)
+                OnAttack();
+            attackCoolDown = 1f / attackSpeed;
+        }
+    }
+
+    IEnumerator DoDamage(CharacterStats stats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        stats.TakeDamage(myStats.damage.GetValue());
+    }
+}
